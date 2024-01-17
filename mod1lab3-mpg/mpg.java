e287c75430fae30ba4a28e00f67eeda674d2af6b
ateschan <addisonteschan@gmail.com>
2024-01-16 22:55:02 -0600

    refactor lines

--- mod1lab3-mpg/mpg.java
+++ mod1lab3-mpg/mpg.java
@@ -1,8 +1,7 @@
 //Addison Teschan
 //January 16 2024
 //Miles per gallon
-//Prompt user for miles driver, and gallons used. Output total miles per gallon.
-
+//Prompt user for miles driven, and gallons used. Output total miles per gallon.
 import java.util.Scanner;
 class mpg{
     public static void main(String[] args){
@@ -11,8 +10,6 @@ class mpg{
         double miles = scan.nextDouble();
         System.out.println("Number of fuel used in gallons: ");
         double gallons = scan.nextDouble();
-
         System.out.println("Miles Per Gallon: " + (miles / gallons) );
-
     }
 }
